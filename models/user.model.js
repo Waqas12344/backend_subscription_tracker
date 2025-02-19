ae4d98d5203da0e327115e604078cf3ab8bbba25
react-redux-project <wm635950@gmail.com>
2025-02-19 11:56:42 +0500

    add models

---
+++ models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"user name is required"],
+        trim:true,
+        minLength:2,
+        maxLength:50,
+    },
+    email:{
+        type:String,
+        required:[true,"user email is required"],
+        unique:true,
+        trim:true,
+        lowercase:true,
+        match:[/\S+@\/S+\.\S+/,"please fill a valid email address"],
+
+    },
+    password:{
+        type:String,
+        required:[true,"user password is required"],
+        trim:true,
+        minLength:6,
+       
+    }
+},{Timestamp:true})
+
+
+const user = mongoose.model("User",userSchema)
+export default user
