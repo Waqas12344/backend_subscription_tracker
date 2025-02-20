2a77a37d1f97361383ddd7c99135c72a53ac0ed3
react-redux-project <wm635950@gmail.com>
2025-02-20 19:07:26 +0500

    impletement arcjet as middleware

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
 import authorize from "./middleware/auth.middleware.js";
+import arcjetMiddleware from "./middleware/arcjet.middleware.js";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
+app.use(arcjetMiddleware)
 
 app.use("/api/v1/auth",authRouter)
 app.use("/api/v1/users",authorize,userRouter)
